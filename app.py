60a9440aea45a03287475a1b6df86a66f12ada1c
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-09-18 05:21:01 +0900

    add route

--- app.py
+++ app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route('/create/')
+def create():
+  pass
+
+@app.route('/edit/')
+def edit():
+  pass
+
+@app.route('/get/<int:dairyNumber>')
+def get(dairyNumber):
+  pass
+
+@app.route('/delete/')
+def delete():
+  pass
+
 if __name__ == '__main__':
   app.run(debug=True, port=5002)
